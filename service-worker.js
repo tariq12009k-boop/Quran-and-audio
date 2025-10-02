8fab5a2bd95b427a52a842f6d4bf8d9379fdc49f
Aends <tariq12009k@gmail.com>
2025-10-02 20:56:11 +0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker installing...");
-});
-self.addEventListener("activate", (event) => {
-  console.log("Service Worker activated!");
-});
-self.addEventListener("fetch", (event) => {
-});
